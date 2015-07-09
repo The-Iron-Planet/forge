68332aa5f4b137ca1078742bc506e886aabf7d7b
John M Buscemi <jmbuscemi@gmail.com>
2015-07-09 17:50:46 -0400

    Add mailer to send email when a new user is created. Works in development only

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-    @user.password = "password"
+    @user.password = "theironyard"
     if @user.save
       redirect_to @user, notice: 'User was successfully created.'
     else
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,15 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :authentication => :plain,
+    :address => "smtp.mailgun.org",
+    :port => 587,
+    :domain => "tiy.forge@gmail.com",
+    user_name: ENV["MAILGUN_USERNAME"],
+    password: ENV["MAILGUN_PASSWORD"]
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
