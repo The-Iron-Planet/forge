f6a3e8e9bd2d0a506213200bd07d292f3ae4db65
John M Buscemi <jmbuscemi@gmail.com>
2015-07-10 08:37:54 -0400

    Add UserController tests. Pass them.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,8 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   setup do
     sign_in users(:one)
-    @user = users(:one)
-    @user.sign_in_count = 2
-    @user.save!
+    @user1 = users(:one)
+    @user2 = users(:two)
   end
 
   test "should get index" do
@@ -21,7 +20,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: {first_name: @user.first_name, last_name: @user.last_name,
+      post :create, user: {first_name: @user1.first_name, last_name: @user1.last_name,
           email: "email@email.com"}
     end
 
@@ -29,26 +28,40 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should show user" do
-    get :show, id: @user
+    get :show, id: @user1
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @user
+    get :edit, id: @user1
     assert_response :success
   end
 
   test "should update user" do
-    patch :update, id: @user, user: { first_name: @user.first_name }
+    patch :update, id: @user1, user: { first_name: @user1.first_name }
     assert_redirected_to user_path(assigns(:user))
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
-      delete :destroy, id: @user
+      delete :destroy, id: @user1
     end
 
     assert_redirected_to users_path
+  end
+
+  test "should get dashboard" do
+    get :dashboard
+    assert_response :success
+    response.body.match(/Forge/)
+  end
+
+  test "should get first login" do
+    sign_out users(:one)
+    sign_in users(:two)
+
+    get :index
+    assert_redirected_to first_login_path
   end
 
   # test "should show user profile picture" do
