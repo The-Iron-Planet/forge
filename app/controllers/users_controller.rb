6cb2e235f53c7abc593ddb33b6befe51920e3420
Joe Reiff <jcreiff@gmail.com>
2015-07-13 10:33:25 -0400

    add company search and prevent empty searches from blowing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,12 @@ class UsersController < ApplicationController
 
     if request.post?
       @users = User.all.ordered.search_results(params[:current_city], params[:current_state],
-          params[:curric_id], params[:campus_id], params[:job_status])
+          params[:curric_id], params[:campus_id], params[:job_status], params[:company_id])
+      if @users == User
+        @users = User.all.ordered
+        flash.now[:notice] = "Please choose specific search parameters."
+        render :index
+      end
     else
       @users = User.all.ordered
     end
