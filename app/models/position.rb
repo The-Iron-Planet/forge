4bbe800bb12674a2d06267c2af65987dee0172e4
Joe Reiff <jcreiff@gmail.com>
2015-07-13 22:01:38 -0400

    remove default scope on position, clean up commented methods on user

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,7 +2,7 @@ class Position < ActiveRecord::Base
   belongs_to :company
   belongs_to :user
 
-  default_scope {order(started_on: :desc)}
+  scope :ordered, -> { order(started_on: :desc) }
 
   def company_name
     company.name_and_location
