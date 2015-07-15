aaf60bc617402a8b5934f100e4b61566b59d0e34
Elizabeth Shealy <elizabeth.shealy@gmail.com>
2015-07-15 18:02:58 -0400

    added modals to courses and curriculum

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20150715041923) do
+ActiveRecord::Schema.define(version: 20150715170410) do
 
   create_table "campuses", force: :cascade do |t|
     t.string   "city"
