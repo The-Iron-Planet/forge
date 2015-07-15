8b62f4ccf7a175527828dfd7a434e64ee091c3bc
John M Buscemi <jmbuscemi@gmail.com>
2015-07-14 23:15:35 -0400

    Add auto_html gem to embed links

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'momentjs-rails', '>= 2.9.0'
 gem 'bootstrap3-datetimepicker-rails', '~> 4.14.30'
 gem 'american_date'
 gem 'faker'
+gem 'auto_html'
 
 
 
--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,9 +1,25 @@
 class Resource < ActiveRecord::Base
+  include AutoHtml
+
   belongs_to :curriculum
   belongs_to :user
 
   scope :ordered, -> { order(updated_at: :desc) }
 
   validates :title, presence: true
+
+  auto_html_for :website do
+    html_escape
+    # image
+    # gist
+    metacafe
+    # google_map
+    ted
+    twitter
+    vimeo
+    youtube(:width => 400, :height => 250, :autoplay => false)
+    link :target => "_blank", :rel => "nofollow"
+    simple_format
+  end
 
 end
