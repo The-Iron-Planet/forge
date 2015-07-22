3595c9e42e70b93e9f96325908387cec866a13de
Patti MacKay <NotoriousPAT@users.noreply.github.com>
2015-07-22 16:59:50 -0400

    change calendar icon and re-align content, change search box color

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,8 +1,14 @@
+// $(document).ready(function() {
+//     $(".nav-ind").mouseenter( function (e) {
+//         $(e.target).addClass('underline'); 
+//         });
+//     $(".nav-ind").mouseleave( function (e) {
+//          $(e.target).removeClass('underline');
+//     });
+// });
+
 $(document).ready(function() {
-    $(".nav-ind").mouseenter( function (e) {
+    $(".nav-ind").onclick( function (e) {
         $(e.target).addClass('underline'); 
         });
-    $(".nav-ind").mouseleave( function (e) {
-         $(e.target).removeClass('underline');
-    });
 });
